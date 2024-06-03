4b2550b7745c60bda9e926eb8a79ad341c4bfa4a
Parth Agrawal <pvagrawal0@gmail.com>
2024-06-03 10:17:22 -0400

    first commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 
+
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -7,35 +8,30 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const Header = () => (
+  <header>
+    <h1>
+      Parth's Site
+    </h1>
+    <nav>
+      <ul>
+        <li>
+          <a href = "/">Home</a>
+        </li>
+        <li>
+          <a href = "/about">About</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+
+      <Header />
+
+     
     </div>
   );
 }
---
+++ app/routes/about.tsx
@@ -0,0 +1,15 @@
+export default function About (){
+    return(
+        <div>
+
+        
+        <h1 className="text-2xl">
+            About    
+        </h1>
+        <p>
+            I'm Parth! I like nature and building things.
+        </p>
+        <img src="/parth.jpeg" alt="Parth" style={{height: 500}}></img>
+        </div>
+    )
+}
