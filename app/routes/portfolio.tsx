1257e55d758ae5503cc882be031387df89872b2e
Parth Agrawal <pvagrawal0@gmail.com>
2024-06-03 12:07:24 -0400

    created portfolio and added links

---
+++ app/routes/portfolio.tsx
@@ -0,0 +1,17 @@
+export default function Portfolio () {
+
+    return(
+        <div>
+            <h1 className="text-3xl font-bold" > 
+            Portfolio
+            </h1>
+
+            <ul className="list-disc">
+                <li> <a href="https://samosaman.itch.io/salami-game" className="underline" >Salami Game</a> - drop salami on a guy's head. That's definitely all that happens.</li>
+                <li> <a href="https://mmm.page/parth_agrawal.applied_cybernetics" className = "underline">Lilypads are solar panels</a></li>
+            </ul>
+
+        </div>
+    )
+
+}
